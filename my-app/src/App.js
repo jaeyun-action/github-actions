4002d86e54ebaae1a31499bc4eaed18e531da496
Jaeyun Jin <busbug3@gmail.com>
2024-05-29 16:50:13 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd4 test
+          Learn GithubAction cicd4 test2
         </a>
       </header>
     </div>
